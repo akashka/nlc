f34f8ef30281eef6970d7530bc6a2c241167d545
Akash Agarwal <akash.kumar@axisrooms.com>
2018-10-11 22:59:09 +0530

    font removal and terms conditions updation

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,13 +4,13 @@ var mongoose = require('mongoose');
 function _init(){
     try{
         // Local
-        // return mongoose.createConnection('mongodb://localhost:27017/nlc');
+        return mongoose.createConnection('mongodb://localhost:27017/nlc');
         
         // Stag
         // return mongoose.createConnection('mongodb://admin:Abcd123$0@ds111963.mlab.com:11963/heroku_8xd7kgcn');
 
         // Live
-        return mongoose.createConnection('mongodb://myUserAdmin:abc123@132.148.16.3:27017/nlc');
+        // return mongoose.createConnection('mongodb://myUserAdmin:abc123@132.148.16.3:27017/nlc');
     }catch(err){
         console.log("No internet connection :(");
     }
