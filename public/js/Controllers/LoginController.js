8f3cc6a8226d322028c74fceed27c1faeaff7320
Akash Agarwal <akash.kumar@axisrooms.com>
2018-11-05 10:28:36 +0530

    single otp and switch levels

--- public/js/Controllers/LoginController.js
+++ public/js/Controllers/LoginController.js
@@ -164,8 +164,8 @@ angular.module('StudentApp.LoginController', [])
 
         $scope.dataSaved = false;
         $scope.tshirtsizeoptions = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
-        $scope.malevels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-        $scope.ttlevels = ["1", "2", "3", "4", "5", "6", "7", "8"];
+        $scope.ttlevels = ["pre", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+        $scope.malevels = ["pre", "1", "2", "3", "4", "5", "6", "7", "8"];
         $scope.eslevels = ["1", "2", "3", "4", "5"];
         $scope.smlevels = ["1", "2", "3", "4", "5", "6"];
 
