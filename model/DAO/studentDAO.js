18d5a9fc26b07d2ce38b42f3e3e1a4181bdafd4a
root <akash.ka01@gmail.com>
2019-11-14 22:58:22 +0530

    change last date, add one date

--- model/DAO/studentDAO.js
+++ model/DAO/studentDAO.js
@@ -290,7 +290,7 @@ function formatDate(date) {
     var d = new Date(date);
     d.setDate(d.getDate());
     var month = '' + (d.getMonth() + 1),
-        day = '' + (d.getDate()),
+        day = '' + (d.getDate() + 1),
         year = d.getFullYear();
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
