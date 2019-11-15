02f3479e09b1281464c7564de044e8c22fad3b55
root <akash.ka01@gmail.com>
2019-11-15 18:45:07 +0530

    date

--- model/DAO/studentDAO.js
+++ model/DAO/studentDAO.js
@@ -292,8 +292,32 @@ function formatDate(date) {
     var month = '' + (d.getMonth() + 1),
         day = '' + (d.getDate() + 1),
         year = d.getFullYear();
+
+    console.log('a', day + ' - ' + month + ' - ' + year);
+    if(month == '1' || month == '3' || month == '5' || month == '7' || month == '8' || month == '10' || month == '12') {
+        if(day == '32') {
+            month = '' + (parseInt(month) + 1);
+            day = '1';
+        }
+        if(month == '13') {
+            month = '1';
+            year = '' + (parseInt(year) + 1);
+        }
+        console.log('b', day + ' - ' + month + ' - ' + year);
+    } else if(month == '4' || month == '6' || month == '9' || month == '11') {
+        if(day == '31') {
+            month = '' + (parseInt(month) + 1);
+            day = '1';
+        }
+        console.log('c', day + ' - ' + month + ' - ' + year);
+    } else if(month == '2' && day == '28') {
+        month = '' + (parseInt(month) + 1);
+        day = '1';
+        console.log('d', day + ' - ' + month + ' - ' + year);
+    }
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
+    console.log('e', day + ' - ' + month + ' - ' + year);
     return [day, month, year].join('/');
 }
 
